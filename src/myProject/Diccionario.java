5d58a57747e8a1768ba74db7d1b410a41f30acfa
202043246 Jose Miguel Becerra Casierra-202043291 Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co-brand.juan@correounivalle.edu.co>
2022-02-18 22:11:34 -0500

    Añadir diseño de clases

--- src/myProject/Diccionario.java
+++ src/myProject/Diccionario.java
@@ -3,15 +3,25 @@ package myProject;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class that contains functions to the dictionary that we took from 'diccionario.txt'
+ *
+ * @version v.1.0.0 date:18/02/2022
+ * @autor Juan Esteban Brand Tovar  brand.juan@correounivalle.edu.co / Jose Miguel Becerra Casierra jose.becerra@correounivalle.edu.co
+ */
+
 public class Diccionario {
     private ArrayList<String> diccionario = new ArrayList<String>();
 
-    public Diccionario(){
+    public Diccionario() {
         FileManager fileManager = new FileManager();
         diccionario = fileManager.lecturaFile();
     }
 
-    public String getFrase(){
+    /**
+     * Function that get a word randomly from the dictionary
+     */
+    public String getFrase() {
         Random aleatorio = new Random();
         return diccionario.get(aleatorio.nextInt(diccionario.size()));
     }
--- src/myProject/Usuarios.java
+++ src/myProject/Usuarios.java
@@ -3,15 +3,25 @@ package myProject;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Class that contains functions to the dictionary that we took from 'usuarios.txt'
+ *
+ * @version v.1.0.0 date:18/02/2022
+ * @autor Juan Esteban Brand Tovar  brand.juan@correounivalle.edu.co / Jose Miguel Becerra Casierra jose.becerra@correounivalle.edu.co
+ */
+
 public class Usuarios {
     private ArrayList<String> usuarios = new ArrayList<String>();
 
-    public Usuarios(){
+    public Usuarios() {
         FileManagerUsuarios fileManagerUsuarios = new FileManagerUsuarios();
         usuarios = fileManagerUsuarios.lecturaFileUsuario();
     }
 
-    public String getFrase(){
+    /**
+     * Function that get a word randomly from the users
+     */
+    public String getUser() {
         Random aleatorio = new Random();
         return usuarios.get(aleatorio.nextInt(usuarios.size()));
     }
